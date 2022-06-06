123b7b09858986cb968d2a3caf646d0b208ee93e
quannguyen03 <94555663+quannguyen03@users.noreply.github.com>
2022-06-06 16:18:54 -0500

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -138,7 +138,7 @@ $(document).ready(function () {
           text: textConfig.text11,
           confirmButtonColor: "#83d0c9",
           onClose: () => {
-            window.location = "https://www.instagram.com/_quannng_";
+            window.location = "https://www.instagram.com/_qhnguyeenx_";
           },
         });
       }
